66e4b3cb33ad4b1d0abb3468d1d0ba13683c14bc
Nich Rosen <nicholasrosen234@gmail.com>
2022-02-27 23:35:35 -0800

    added test classes

--- LinkedBagTest.java
+++ LinkedBagTest.java
@@ -1,31 +1,31 @@
 import java.util.Arrays;
 
-public class ResizableArrayBagTest{
+public class LinkedBagTest{
 
     public static void main(String[] args){
 
-        BagInterface<String> bag_1 = new ResizableArrayBag<>();
-        BagInterface<String> bag_2 = new ResizableArrayBag<>();
+        BagInterface<String> bag_1 = new LinkedBag<>();
+        BagInterface<String> bag_2 = new LinkedBag<>();
 
         bag_1.add("A");
-        bag_1.add("Q");
-        bag_1.add("Q");
-        bag_1.add("R");
-        bag_1.add("X");
-        bag_1.add("Z");
+        bag_1.add("B");
+        bag_1.add("C");
+        bag_1.add("D");
+        bag_1.add("D");
+        bag_1.add("D");
 
         bag_2.add("A");
         bag_2.add("A");
-        bag_2.add("U");
-        bag_2.add("X");
-        bag_2.add("Y");
-        bag_2.add("Y");
+        bag_2.add("H");
+        bag_2.add("I");
+        bag_2.add("I");
+        bag_2.add("J");
         bag_2.add("Y");
         bag_2.add("Y");
         bag_2.add("Z");
 
-        System.out.println("Bag1 uses a resizable array bag and contains the following: [A, Q, Q, R, X, Z]");
-        System.out.println("Bag2 uses a resizable array bag and contains the following: [A, A, U, X, Y, Y, Y, Y, Z]");
+        System.out.println("Bag1 uses a linked bag and contains the following: [A, B, C, D, D, D]");
+        System.out.println("Bag2 uses a linked array bag and contains the following: [A, A, H, I, I, J, Y, Y, Z]");
         System.out.println("Applying the union method with Bag2 as the parameter: " + Arrays.toString(bag_1.union(bag_2).toArray()));
         System.out.println("Applying the intersection method with Bag2 as the parameter: " + Arrays.toString(bag_1.intersection(bag_2).toArray()));
         System.out.println("Applying the difference method with Bag2 as the parameter: " + Arrays.toString(bag_1.difference(bag_2).toArray()));
